d5daaf559d29f5fd9754eaaba7faca3c01a76da0
Rex Flores <Rexgflores@gmail.com>
2021-01-03 15:50:11 -0800

    added bands, replies and fixed background

--- script.js
+++ script.js
@@ -1,50 +1,190 @@
-var coolBands = ["gorillaz", "tribe called quest", "a tribe called quest", "neon indian", "funkadelic", "parliament", "beastie boys", "the beastie boys", "b.e.a.s.t.i.e boys","the b.e.a.s.t.i.e boys", "kanye west", "pusha-t", "pusha t", "pushat","tyler", "tyler, the creator", "tyler the creator", "prince", "prince and the revolution", "prince + the revolution", "prince & the revolution", "the revolution", "neko case", "sam cooke", "yellow magic orchestra", "cut copy", "beck", "modest mouse", "bts", "the grateful dead", "kanye", "ye", "yeezy", "yeezus", "talking heads", "the talking heads", "kendrick", "kendrick lamar", "k dot", "k-dot", "thundercat", "thunder cat", "kamasi washington", "flying lotus", "flylo", "doom", "mf doom", "m.f. doom", "metal face doom", "m.f.-doom", "mf-doom", "katy perry", "beulah", "centro-matic", "centromatic", "centro matic", "sergio leone","danger mouse", "dangermouse", "paul simon", "paulsimon", "rtj", "run the jewels", "runthejewels", "killer mike and el-p", "killer mike and el p", "killer-mike and el-p", "killer mike", "kiler-mike", "el-p", "el p", "elp", "van halen", "vanhalen"]
-var hellaCool = ["red hot chili peppers", "rhcp", "sublime", "weezer", "green day", "eve6", "eve 6", "reel big fish", "real big fish"]
-var howOriginal = ["the beatles", "the rolling stones", "radiohead", "beyonce", "michael jackson", "bob dylan", "the who", "eagles", "the eagles", "marvin gaye", "taylor swift", "t-swizzle", "t swizzle", "tom petty", "tom petty and the heartbreakers", "tom petty + the heartbreakers", "santana", "carlos santana", "clash", "the clash" ]
-var notMusic = ["death grips", "death-grips", "deathgrips"]
+var coolBands = [
+  "gorillaz",
+  "tribe called quest",
+  "a tribe called quest",
+  "neon indian",
+  "funkadelic",
+  "parliament",
+  "beastie boys",
+  "the beastie boys",
+  "b.e.a.s.t.i.e boys",
+  "the b.e.a.s.t.i.e boys",
+  "kanye west",
+  "pusha-t",
+  "pusha t",
+  "pushat",
+  "tyler",
+  "tyler, the creator",
+  "tyler the creator",
+  "prince",
+  "prince and the revolution",
+  "prince + the revolution",
+  "prince & the revolution",
+  "the revolution",
+  "neko case",
+  "sam cooke",
+  "yellow magic orchestra",
+  "cut copy",
+  "beck",
+  "modest mouse",
+  "bts",
+  "the grateful dead",
+  "kanye",
+  "ye",
+  "yeezy",
+  "yeezus",
+  "talking heads",
+  "the talking heads",
+  "kendrick",
+  "kendrick lamar",
+  "k dot",
+  "k-dot",
+  "thundercat",
+  "thunder cat",
+  "kamasi washington",
+  "flying lotus",
+  "flylo",
+  "doom",
+  "mf doom",
+  "m.f. doom",
+  "metal face doom",
+  "m.f.-doom",
+  "mf-doom",
+  "katy perry",
+  "beulah",
+  "centro-matic",
+  "centromatic",
+  "centro matic",
+  "sergio leone",
+  "danger mouse",
+  "dangermouse",
+  "paul simon",
+  "paulsimon",
+  "rtj",
+  "run the jewels",
+  "runthejewels",
+  "killer mike and el-p",
+  "killer mike and el p",
+  "killer-mike and el-p",
+  "killer mike",
+  "kiler-mike",
+  "el-p",
+  "el p",
+  "elp",
+  "van halen",
+  "vanhalen",
+  "eddie van halen",
+  "eddie vanhalen",
+  "aretha franlin",
+  "arethafranklin",
+  "hot dad"
+];
+var hellaCool = [
+  "red hot chili peppers",
+  "rhcp",
+  "sublime",
+  "weezer",
+  "green day",
+  "eve6",
+  "eve 6",
+  "reel big fish",
+  "real big fish",
+];
+var howOriginal = [
+  "the beatles",
+  "the rolling stones",
+  "radiohead",
+  "beyonce",
+  "michael jackson",
+  "bob dylan",
+  "the who",
+  "eagles",
+  "the eagles",
+  "marvin gaye",
+  "taylor swift",
+  "t-swizzle",
+  "t swizzle",
+  "tom petty",
+  "tom petty and the heartbreakers",
+  "tom petty + the heartbreakers",
+  "santana",
+  "carlos santana",
+  "clash",
+  "the clash",
+];
+var notMusic = ["death grips", "death-grips", "deathgrips"];
 
-var genReplies = ["Pffft! They haven't made a good record in years, if ever.", "I don't pay attention to minor work.", "I don't think you know what music is.", "Broh, do you even listen to music?","Am I supposed to take you seriously?", "Yeah, I think NPR said they were cool.", "I don't listen to that because I only listen to music that no one else has heard of.", "I don't listen to commercial music.", "Sure, they have maybe one good album.", "No one would upvote that opinion.", "How pedestrian, tsk tsk..."]
-var coolReplies = ["That is most excellent music! You are a being of impeccable taste.", "One of the greats.", "Yeah, you get it.", "Now I know that's right.", "That is most definitely where it's at.", "That's my jam!", "You're not the first person to tell me that. I need to listen to more.", "Me and my friends love that shit!"]
-var hellaReplies = ["Wowee, they are hella cool! You must be hella cool for listening to them.", "Man, I used to listen to that before I had pubes.", "I danced to that in middle school, fer sure.", "Hella tight!", "I'm embarassed to listen to any of that, and you should be too."]
-var originalReplies = ["How original.", "Yeah, my parents love that music.", "Cool, cool, I think I've heard like, one or two songs.", "You know there is new music, right?", "Cool, so do you only listen to what's on the radio?", "Yeah, that's good for when you're at the grocery store, or waiting at the DMV.", "Sure, but who doesn't like that?"]
-var notMusicReply = "That's not music."
+var genReplies = [
+  "Pffft! They haven't made a good record in years, if ever.",
+  "I don't pay attention to minor work.",
+  "I don't think you know what music is.",
+  "Broh, do you even listen to music?",
+  "Am I supposed to take you seriously?",
+  "Yeah, I think NPR said they were cool.",
+  "I don't listen to that because I only listen to music that no one else has heard of.",
+  "I don't listen to commercial music.",
+  "Sure, they have maybe one good album.",
+  "No one would upvote that opinion.",
+  "How pedestrian, tsk tsk...",
+];
+var coolReplies = [
+  "That is most excellent music! You are a being of impeccable taste.",
+  "One of the greats.",
+  "Yeah, you get it.",
+  "Now I know that's right.",
+  "That is most definitely where it's at.",
+  "That's my jam!",
+  "You're not the first person to tell me that. I need to listen to more.",
+  "Me and my friends love that shit!",
+  "All right, you really know your stuff."
+];
+var hellaReplies = [
+  "Wowee, they are hella cool! You must be hella cool for listening to them.",
+  "Man, I used to listen to that before I had pubes.",
+  "I danced to that in middle school, fer sure.",
+  "Hella tight!",
+  "I'm embarassed to listen to any of that, and you should be too.",
+];
+var originalReplies = [
+  "How original.",
+  "Yeah, my parents love that music.",
+  "Cool, cool, I think I've heard like, one or two songs.",
+  "You know there is new music, right?",
+  "Cool, so do you only listen to what's on the radio?",
+  "Yeah, that's good for when you're at the grocery store, or waiting at the DMV.",
+  "Sure, but who doesn't like that?",
+  "Does that have grampa guitars? I think that's grampa music."
+];
+var notMusicReply = "That's not music.";
 
-var $submit = $("#submit")
-Array.prototype.sample = function(){
-    return this[Math.floor(Math.random()*this.length)];
+var $submit = $("#submit");
+Array.prototype.sample = function () {
+  return this[Math.floor(Math.random() * this.length)];
+};
+var submitBand = function () {
+  var bandInput = $("#name_field").val().trim().toLowerCase();
+  var bandReply = bandInput + "? <br>";
+  if (bandInput === "") {
+    return;
+  } else if (coolBands.includes(bandInput)) {
+    bandReply += coolReplies.sample();
+  } else if (hellaCool.includes(bandInput)) {
+    bandReply += hellaReplies.sample();
+  } else if (howOriginal.includes(bandInput)) {
+    bandReply += originalReplies.sample();
+  } else if (notMusic.includes(bandInput)) {
+    bandReply += notMusicReply;
+  } else {
+    bandReply += genReplies.sample();
   }
-var submitBand = function(){
-    var bandInput = $("#name_field").val().trim().toLowerCase()
-    var bandReply = bandInput + "? <br>"
-    if (bandInput=== "") {
-        return
-    }
-    
-    else if (coolBands.includes(bandInput)){
-       bandReply += (coolReplies.sample())
-    }
-    else if (hellaCool.includes(bandInput)){
-        bandReply += (hellaReplies.sample())
-    }
-    else if (howOriginal.includes(bandInput)){
-        bandReply += (originalReplies.sample())
-    }
-    else if (notMusic.includes(bandInput)){
-        bandReply += (notMusicReply)
-    }
-    else { 
-        bandReply += (genReplies.sample())
-    }
-    $("#snark-tank").html(bandReply)
-    
-    $('#name_field').val("");
-    
+  $("#snark-tank").html(bandReply);
 
-} 
-$submit.click(submitBand)
+  $("#name_field").val("");
+};
+$submit.click(submitBand);
 
-$("#name_field").keydown(function(event){
-    if (event.which == 13){
-        submitBand()
-    }
-})
+$("#name_field").keydown(function (event) {
+  if (event.which == 13) {
+    submitBand();
+  }
+});
